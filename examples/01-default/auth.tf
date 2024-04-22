de5c2599e737abece80c2890f53aa5378baa3d38
Ledermayer <18106157+Ledermayer@users.noreply.github.com>
2024-04-22 18:03:54 +0200

    var to provider block

--- examples/01-default/auth.tf
+++ examples/01-default/auth.tf
@@ -7,9 +7,13 @@ provider "azurerm" {
   }
 }
 
-# provider "oci" {
-#   region = "eu-frankfurt-1"
-# }
+provider "oci" {
+  tenancy_ocid = var.TENANCY_OCID
+  user_ocid    = var.USER_OCID
+  private_key  = var.PRIVATE_KEY
+  fingerprint  = var.FINGERPRINT
+  region       = "eu-frankfurt-1"
+}
 
 #Versions
 terraform {
