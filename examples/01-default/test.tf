cc8bf4f12c9b02548022e8324379678fed79d7c8
Ledermayer <18106157+Ledermayer@users.noreply.github.com>
2024-04-22 18:08:32 +0200

    no defined tags

--- examples/01-default/test.tf
+++ examples/01-default/test.tf
@@ -6,9 +6,9 @@ module "terratest-network-vcn" {
   vcn_display_name   = "terratest-vcn"
   vcn_cidr_blocks    = ["172.20.0.0/16", "172.21.0.0/16"]
   vcn_dns_label      = "terratest"
-  vcn_defined_tags = {
-    "CreatedBy" = "Terraform"
-  }
+  # vcn_defined_tags = {
+  #   "CreatedBy" = "Terraform"
+  # }
   vcn_freeform_tags = {
     "Terraform" = "True"
   }
