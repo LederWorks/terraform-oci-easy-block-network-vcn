2f8ff2e38e0f4f5f4fd3159313991cfa8584dbd9
Ledermayer <18106157+Ledermayer@users.noreply.github.com>
2024-04-23 02:59:35 +0200

    vcn_subnets

--- variables.tf
+++ variables.tf
@@ -125,7 +125,7 @@ variable "vcn_subnets" {
   EOT
   #Validation that dns_label must be unique across all subnets
   validation {
-    condition     = length(var.example_map) == 0 || length(distinct(values([for v in var.example_map : v.dns_label if v.dns_label != null]))) == length([for v in var.example_map : v.dns_label if v.dns_label != null])
+    condition     = length(var.vcn_subnets) == 0 || length(distinct(values([for v in var.vcn_subnets : v.dns_label if v.dns_label != null]))) == length([for v in var.vcn_subnets : v.dns_label if v.dns_label != null])
     error_message = "The dns_label must be unique across all objects in the map."
   }
 }
