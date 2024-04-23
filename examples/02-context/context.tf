b7dae76c13dd640ee7169cb3cf14ef83f20d9099
Ledermayer <18106157+Ledermayer@users.noreply.github.com>
2024-04-23 03:57:59 +0200

    o6

--- examples/02-context/context.tf
+++ examples/02-context/context.tf
@@ -1,12 +1,11 @@
 module "context" {
-  source = "git::https://github.com/LederWorks/terraform-generic-easy-context.git?ref=v0.5.0"
+  source = "git::https://github.com/LederWorks/terraform-generic-easy-context.git?ref=8671c05fa1162c46cacb25259f6ba251d8819973"
 
 
   subsidiary  = "candy"
   cloud       = "oci"
   environment = "int"
-  # region      = "eu-frankfurt-1"
-  region  = "westeurope"
+  region      = "eu-frankfurt-1"
   project     = "form"
   department  = "pd"
 
